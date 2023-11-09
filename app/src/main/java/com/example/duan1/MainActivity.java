8ca1d57f918cb0be9e71e6700275bfee5e49ca1d
minhpq1910 <pqminh20041910@gmail.com>
2023-11-09 19:22:54 +0700

    code mới

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String hi = "code mới";
+        //pham quang minh
     }
 }
