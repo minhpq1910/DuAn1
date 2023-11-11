dc16927e5c5cecd3427b41025cfab944f0802b3e
NawHuHi <namncph38048@fpt.edu.vn>
2023-11-11 19:22:59 +0700

    moi

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String hi = "code mới";
-        //pham quang minh
-
-        //hello
+        String hi = "hihi";
     }
 }
